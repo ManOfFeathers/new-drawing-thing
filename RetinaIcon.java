6fb53ffb1906febfcf5bfd1c03e1032fe8a0b6e2
kobejean <kobejean@me.com>
2017-05-27 14:20:03 -0600

    Commented annoying "printStackTrace()"

--- RetinaIcon.java
+++ RetinaIcon.java
@@ -40,7 +40,7 @@ public class RetinaIcon extends ImageIcon {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            // e.printStackTrace();
         }
         return isRetina;
     }
