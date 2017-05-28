9e5aa50c7e753acee505844bc6f20a1ab0f31057
ManOfFeathers <griffintmyers@gmail.com>
2017-05-27 18:48:32 -0600

    Replaced z with "depth"

--- NewDrawing.java
+++ NewDrawing.java
@@ -26,38 +26,55 @@ public class NewDrawing {
 		ArrayList<Line3D> greenLines = new ArrayList<Line3D>();
 		ArrayList<Line3D> yellowLines = new ArrayList<Line3D>();
 
-		int space = 500;
+		int space = 250;
 
 		int z = 0;
 
-		for (int m = 0; m < 500; m++) {
+		for (int m = 0; m < space; m++) {
 			Coord3D coord1 = new Coord3D(0, 0, m * depth / space);
-			Coord3D coord2 = new Coord3D(0, width - (m * height / space), z);
+			Coord3D coord2 = new Coord3D(0, m * height / space, depth);
 
-			Coord3D coord3 = new Coord3D(m * width / space, height, z);
+			Coord3D coord3 = new Coord3D(width - (m * width / space), height, depth);
 			Coord3D coord4 = new Coord3D(width, height, m * depth / space);
 			
 			Coord3D coord5 = new Coord3D(0, 0, m * height / space);
-			Coord3D coord6 = new Coord3D(width - (m * height / space), 0, z);
+			Coord3D coord6 = new Coord3D(m * width / space, 0, depth);
 			
-			Coord3D coord7 = new Coord3D(width, m * width / space, z);
+			Coord3D coord7 = new Coord3D(width, height - (m * height / space), depth);
 			Coord3D coord8 = new Coord3D(width, height, m * depth / space);
 			
-			Coord3D coord9 = new Coord3D(width - (m * width / space), height, z);
-			Coord3D coord10 = new Coord3D(0, height, m * depth / space);			
+			Coord3D coord9 = new Coord3D(m * width / space, height, depth);
+			Coord3D coord10 = new Coord3D(0, height, m * depth / space);
+			
+			Coord3D coord11 = new Coord3D(0, height - (m * height / space), depth);
+			Coord3D coord12 = new Coord3D(0, height, m * depth / space);
+			
+			Coord3D coord13 = new Coord3D(width - (m * width / space), 0, depth);
+			Coord3D coord14 = new Coord3D(width, 0, m * depth / space);	
+			
+			Coord3D coord15 = new Coord3D(width, m * width / space, depth);
+			Coord3D coord16 = new Coord3D(width, 0, m * depth / space);	
 		
 			Line3D line1 = new Line3D(coord1, coord2);
 			Line3D line2 = new Line3D(coord3, coord4);
+			
 			Line3D line3 = new Line3D(coord5, coord6);
 			Line3D line4 = new Line3D(coord7, coord8);
 			
 			Line3D line5 = new Line3D(coord9, coord10);
+			Line3D line6 = new Line3D(coord11, coord12);
+			
+			Line3D line7 = new Line3D(coord13, coord14);
+			Line3D line8 = new Line3D(coord15, coord16);
 		
 			blueLines.add(line1);
 			redLines.add(line2);
 			greenLines.add(line3);
 			yellowLines.add(line4);
 			greenLines.add(line5);
+			yellowLines.add(line6);
+			redLines.add(line7);
+			blueLines.add(line8);
 		}
 
 		for (Line3D line1 : blueLines) {
